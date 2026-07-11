19b3e8b0e685239a423e5723779177f86aa66dfd
jaspal.dpdp <jaspal.singh@dpdpconsultants.com>
2026-07-12 01:20:34 +0530

    feat: add Couchbase database connector

--- couchbase_connector.py
+++ couchbase_connector.py
@@ -1,12 +1,33 @@
+"""Couchbase connector using couchbase SDK."""
+
+import os
+
 from couchbase.cluster import Cluster
 from couchbase.auth import PasswordAuthenticator
 
-def connect():
-    auth = PasswordAuthenticator("<username>", "<password>")
-    cluster = Cluster("couchbase://localhost", authenticator=auth)
+
+def connect() -> Cluster:
+    """Connect to Couchbase.
+
+    Environment variables:
+        COUCHBASE_HOST: Couchbase server hostname (default: localhost)
+        COUCHBASE_USERNAME: Authentication username
+        COUCHBASE_PASSWORD: Authentication password
+        COUCHBASE_BUCKET: Bucket name (used in example)
+    """
+    host = os.environ.get('COUCHBASE_HOST', 'localhost')
+    username = os.environ.get('COUCHBASE_USERNAME', '<username>')
+    password = os.environ.get('COUCHBASE_PASSWORD', '<password>')
+    auth = PasswordAuthenticator(username, password)
+    cluster = Cluster(f"couchbase://{host}", authenticator=auth)
     return cluster
 
+
 if __name__ == "__main__":
-    cluster = connect()
-    bucket = cluster.bucket("<bucket>")
-    print(f"Couchbase connected. Bucket: {bucket.name}")
+    try:
+        cluster = connect()
+        bucket_name = os.environ.get('COUCHBASE_BUCKET', '<bucket>')
+        bucket = cluster.bucket(bucket_name)
+        print(f"Couchbase connected. Bucket: {bucket.name}")
+    except Exception as e:
+        print(f"Failed to connect to Couchbase: {e}")
