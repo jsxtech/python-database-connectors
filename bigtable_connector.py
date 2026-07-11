d7a5c04cea27f6c2e71d26f7a06f5336407e73c8
jaspal.dpdp <jaspal.singh@dpdpconsultants.com>
2026-07-12 01:20:34 +0530

    feat: add Google Bigtable connector

--- bigtable_connector.py
+++ bigtable_connector.py
@@ -1,11 +1,30 @@
-from google.cloud import bigtable
+"""Google Cloud Bigtable connector using google-cloud-bigtable."""
 
-def connect():
-    client = bigtable.Client(project="<project-id>", admin=True)
-    instance = client.instance("<instance-id>")
+import os
+
+from google.cloud import bigtable
+from google.cloud.bigtable.instance import Instance
+
+
+def connect() -> Instance:
+    """Connect to Google Cloud Bigtable.
+
+    Environment variables:
+        BIGTABLE_PROJECT: GCP project ID
+        BIGTABLE_INSTANCE: Bigtable instance ID
+    """
+    client = bigtable.Client(
+        project=os.environ.get('BIGTABLE_PROJECT', '<project-id>'),
+        admin=True
+    )
+    instance = client.instance(os.environ.get('BIGTABLE_INSTANCE', '<instance-id>'))
     return instance
 
+
 if __name__ == "__main__":
-    instance = connect()
-    tables = instance.list_tables()
-    print(f"Bigtable connected. Tables: {[t.table_id for t in tables]}")
+    try:
+        instance = connect()
+        tables = instance.list_tables()
+        print(f"Bigtable connected. Tables: {[t.table_id for t in tables]}")
+    except Exception as e:
+        print(f"Error connecting to Bigtable: {e}")
